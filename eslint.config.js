f1e8d4bc6c87da79badf20fb0d5b584323770959
Dean Gilmore <dean@videoremix.io>
2025-08-14 04:41:30 +0000

    chore: finalize env example, add secret scan workflow, relax lint scope (ignore backups/assets)

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,16 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: [
+      'dist',
+      // Exclude non-source bundles/backups and large attached assets
+      'backup_pre_friday-1/**',
+      'attached_assets/**',
+      'extracted/**',
+      'temp_zip_contents/**',
+      'pipeline_repo/**',
+      'pipeline_deals_new/**',
+    ] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
